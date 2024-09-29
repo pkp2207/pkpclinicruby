2542e5fdd4399da14b761be70a722fb6cb3aebbe
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-09-29 10:14:15 +0530

    Update 2

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,24 @@
+class WelcomeController < ApplicationController
+    def index
+      # Logic for your home page (if necessary)
+    end
+  
+    def manifest
+      render json: {
+        "short_name": "ClinicApp",
+        "name": "Clinic Application",
+        "icons": [
+          {
+            "src": "/icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff"
+      }
+    end
+  end
+  
